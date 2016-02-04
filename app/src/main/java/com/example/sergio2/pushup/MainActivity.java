64a7eb27a639fc191ecff9e041b5ec56caf2e025
sergiobs <bajo.sergio@gmail.com>
2016-02-04 21:58:19 +0100

    Modified MainActivity Added new File TestAddFile.java

--- app/src/main/java/com/example/sergio2/pushup/MainActivity.java
+++ app/src/main/java/com/example/sergio2/pushup/MainActivity.java
@@ -38,5 +38,9 @@ public class MainActivity extends ActionBarActivity {
     private void TestUpoad()
     {
         int i = 1+1;
+
+        int ii = 2+2;
+
+        int iii = i+ii;
     }
 }
---
+++ app/src/main/java/com/example/sergio2/pushup/TestAddFile.java
@@ -0,0 +1,7 @@
+package com.example.sergio2.pushup;
+
+/**
+ * Created by sergio2 on 04/02/2016.
+ */
+public class TestAddFile {
+}
